e106e6cccdf709096c9dada91226156c0d4089f8
Greg Lixandrão <contato@greglixandrao.com>
2022-11-04 01:12:41 -0300

    fix main.c with includes

--- testes/main.c
+++ testes/main.c
@@ -6,11 +6,14 @@
 /*   By: gtreviza <gtreviza@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 06:52:35 by gtreviza          #+#    #+#             */
-/*   Updated: 2022/10/27 06:53:25 by gtreviza         ###   ########.fr       */
+/*   Updated: 2022/11/04 01:12:04 by gtreviza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "get_next_line.h"
 
 int	main(void)
@@ -21,6 +24,9 @@ int	main(void)
 
 	fd = open("test_text.txt", O_RDONLY);
 	line = "";
+	/*line = get_next_line(fd);
+	printf("line[%d]: %s\n", i, line);
+	free(line);*/
 	i = 1;
 	while (line)
 	{
